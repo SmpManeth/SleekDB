ebc52dc2202ba1ed3a850b703c78f5d2a1c064ba
Maneth Pathirana <manith.sampath@towcha.com.au>
2022-09-11 17:46:29 +0530

    Code Oranized with some Comments to understand

--- index.php
+++ index.php
@@ -1,13 +1,19 @@
 <?php
+// has the functions like store, findall, deletestore etc..
 include 'vendor/rakibtg/sleekdb/src/SleekDB.php';
 
+//directory of the databse to store the tables and stuff
 $dataDir = __DIR__ . "/myDatabase";
+
+ //using the Store Function to Create a table
 use SleekDB\Store;
+
+//Creating new Stores
 $newsStore = new Store('news', $dataDir);
 $userStore = new Store('users', $dataDir);
 $postStore = new Store('posts', $dataDir);
 
-
+//making Json Type data to add to the table
 $user=[
     'name'=> 'Maneth Pathirana',
     'about' => 'smpmaneth@gmail.com',
@@ -16,20 +22,13 @@ $user=[
         'lg' => "/img-lg.jpg"
     ]
 ];
+//adding above data to the Users table
 $userStore->insert($user);
 
-$article = [
-    "title" => "Google Pixel XL",
-    "about" => "Google announced a new Pixel!",
-    "author" => [
-        "avatar" => "profile-12.jpg",
-        "name" => "Foo Bar",
-    ],
-];
-// $results = $newsStore->insert($article);
-
+//deleting a table(store)
 $newsStore->deleteStore();
 
+//fetch all the data from a store(table)
 $allNews = $userStore->findAll();
 
 print_r(count($allNews));
